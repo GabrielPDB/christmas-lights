8de37ca4265af94fa12e450bba58c5c9e935c82f
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2021-09-16 07:32:08 -0300

    almost done

--- scripts.js
+++ scripts.js
@@ -1,22 +1,36 @@
 const lights = document.querySelectorAll('.light')
+
+const Lights = {
+  setAnimationDuration() {
+    let animationDuration = document.querySelector('#duration').value
+
+    lights.forEach((light, index) => {
+      light.style['animation-duration'] = animationDuration + 'ms'
+      if (index % 2 == 0) {
+        light.style['animation-delay'] = animationDuration / 2 + 'ms'
+      }
+    })
+  },
+  turnOn() {
+    lights.forEach(light => {
+      light.classList.remove('inative')
+    })
+  },
+  turnOff() {
+    lights.forEach(light => {
+      light.classList.add('inative')
+    })
+  }
+}
+
 const playButton = document.querySelector('#play')
 const stopButton = document.querySelector('#stop')
 
 playButton.addEventListener('click', () => {
-  lights.forEach(light => {
-    light.classList.remove('inative')
-  })
+  Lights.turnOn()
 })
 stopButton.addEventListener('click', () => {
-  lights.forEach(light => {
-    light.classList.add('inative')
-  })
+  Lights.turnOff()
 })
 
-const setIntervalOfLights = () => {
-  let animationDuration = document.querySelector('#interval').value
-
-  lights.forEach(light => {
-    light.style['animation-duration'] = animationDuration + 'ms'
-  })
-}
+/* Refatorar animação de ligar e desligar lights */
